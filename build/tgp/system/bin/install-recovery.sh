1e3d055d26309886ce1f6c569f6f1804508c3f00
djb77 <dwayne.bakewell@gmail.com>
2016-11-10 22:50:52 +1100

    G935FXXU1BPJG Firmware
    TGP v7.xx Base Firmware - Deodexed and Zipaligned
    Prepared with SuperR's Kitchen v1.1.1.9
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/tgp/system/bin/install-recovery.sh
+++ build/tgp/system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39796736:e22297f95aced58b654f6056ac41834ab5e37b62; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37523456:a260da33a78426adf0e83599737ad6a995ccc9cd EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY e22297f95aced58b654f6056ac41834ab5e37b62 39796736 a260da33a78426adf0e83599737ad6a995ccc9cd:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39854080:527d52553b8f87cd6a20804f0058e64dcf51549c; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37578752:e3b2a487ccad2c5e94ad75fb53859578a6566f87 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 527d52553b8f87cd6a20804f0058e64dcf51549c 39854080 e3b2a487ccad2c5e94ad75fb53859578a6566f87:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
