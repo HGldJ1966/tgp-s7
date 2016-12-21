e4d9e127a3e8e4b87ee82228bb01a0c5026304bf
djb77 <dwayne.bakewell@gmail.com>
2016-12-22 09:38:26 +1100

    G935FXXU1BPLB Firmware
    TGP v8.2x Base Firmware: G935FXXU1BPLB / G930FXXU1BPLB
    Prepared with SuperR's Kitchen v1.1.2.8
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- build/tgp/system/bin/install-recovery.sh
+++ build/tgp/system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39854080:9733f83e5f8c4aae3d746e3301188ec770b25a6e; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37578752:ba19018bfc93a0757336043b08a3d8ecedadd538 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 9733f83e5f8c4aae3d746e3301188ec770b25a6e 39854080 ba19018bfc93a0757336043b08a3d8ecedadd538:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39854080:4406ddf5277ddc8dd78acb279fd15937e0dd6e31; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37578752:70f13df779c08a817e1bc9b956903731ff655355 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 4406ddf5277ddc8dd78acb279fd15937e0dd6e31 39854080 70f13df779c08a817e1bc9b956903731ff655355:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
